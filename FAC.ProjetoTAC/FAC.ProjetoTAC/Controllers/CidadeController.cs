199c9e78e2a2676312a13bcc3aca4d643af0b054
Fabio Andrucioli Carnesecca <fabio.carnesecca@smarpad.com.br>
2015-04-29 00:41:52 -0300

    casa

---
+++ FAC.ProjetoTAC/FAC.ProjetoTAC/Controllers/CidadeController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fabio.ProjetoTAC.Models;
+using FAC.ProjetoTAC.Models;
+
+namespace FAC.ProjetoTAC.Controllers
+{
+    public class CidadeController : Controller
+    {
+        private ConectionDBContext db = new ConectionDBContext();
+
+        //
+        // GET: /Cidade/
+
+        public ActionResult Index()
+        {
+            return View(db.Cidades.ToList());
+        }
+
+        //
+        // GET: /Cidade/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Cidade cidade = db.Cidades.Find(id);
+            if (cidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cidade);
+        }
+
+        //
+        // GET: /Cidade/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Cidade/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Cidade cidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cidades.Add(cidade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cidade);
+        }
+
+        //
+        // GET: /Cidade/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Cidade cidade = db.Cidades.Find(id);
+            if (cidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cidade);
+        }
+
+        //
+        // POST: /Cidade/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Cidade cidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cidade).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cidade);
+        }
+
+        //
+        // GET: /Cidade/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Cidade cidade = db.Cidades.Find(id);
+            if (cidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cidade);
+        }
+
+        //
+        // POST: /Cidade/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cidade cidade = db.Cidades.Find(id);
+            db.Cidades.Remove(cidade);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FAC.ProjetoTAC/FAC.ProjetoTAC/Models/ConectionDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using Fabio.ProjetoTAC.Models;
+
+namespace FAC.ProjetoTAC.Models
+{
+    public class ConectionDBContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, add the following
+        // code to the Application_Start method in your Global.asax file.
+        // Note: this will destroy and re-create your database with every model change.
+        // 
+        // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<FAC.ProjetoTAC.Models.ConectionDBContext>());
+
+        public ConectionDBContext() : base("name=ConectionDBContext")
+        {
+        }
+
+        public DbSet<Cidade> Cidades { get; set; }
+    }
+}
